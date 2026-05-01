490e44b01066a78643bb2a085d4eaf0e611c95c7
Or Blank <your-email@example.com>
2026-05-01 12:16:31 +0300

    main def is now defined

--- Day03/Dilutions_calculator.py
+++ Day03/Dilutions_calculator.py
@@ -26,47 +26,48 @@ def Calculate_V2 (C1, V1, C2):
         V2 = (C1 * V1) / C2
         return V2
 
+# Now this is the main function to run the program of the above sub defs
 def main():
-    print("--------------------------------------------------")
+    print("---------------------------------------------------")
     print("Dilution Calculator (C1 * V1 = C2 * V2)")
     print("Keep in mind: All the factors must be in the same units! (Concentration: Molarity, Volume: uL, mL, etc.)")
-    print("--------------------------------------------------")
+    print("---------------------------------------------------")
     print("Hello, Which variable would you like to calculate today?")
     print("1 = C1 (Initial concentration)")
     print("2 = V1 (Initial volume)")
     print("3 = C2 (Final concentration)")
     print("4 = V2 (Final volume)")
-
-choice = input("Enter 1, 2, 3, or 4: ")
-
-if choice == "1":
-    V1 = float(input("Enter V1: "))
-    C2 = float(input("Enter C2: "))
-    V2 = float(input("Enter V2: "))
-    C1 = Calculate_C1(C2, V2, V1)
-    print(f"\nC1 = {C1}")
-    print("This means that the initial concentration of your solution is", C1, "Molarity units.")
-
-elif choice == "2":
-    C1 = float(input("Enter C1: "))
-    C2 = float(input("Enter C2: "))
-    V2 = float(input("Enter V2: "))
-    V1 = Calculate_V1(C1, C2, V2)
-    print(f"\nV1 = {V1}")
-    print("This means that the initial volume of your solution is", V1, "volume units.")
-
-elif choice == "3":
-    C1 = float(input("Enter C1: "))
-    V1 = float(input("Enter V1: "))
-    V2 = float(input("Enter V2: "))
-    C2 = Calculate_C2(C1, V1, V2)
-    print(f"\nC2 = {C2}")
-    print("This means that the final concentration of your solution is", C2, "Molarity units.")
-
-elif choice == "4":
-    C1 = float(input("Enter C1: "))
-    V1 = float(input("Enter V1: "))
-    C2 = float(input("Enter C2: "))
-    V2 = Calculate_V2(C1, V1, C2)
-    print(f"\nV2 = {V2}")
-    print("This means that the final volume of your solution is", V2, "Volume units.")
+    
+    choice = input("Enter 1, 2, 3, or 4: ")
+    
+    if choice == "1":
+        V1 = float(input("Enter V1: "))
+        C2 = float(input("Enter C2: "))
+        V2 = float(input("Enter V2: "))
+        C1 = Calculate_C1(C2, V2, V1)
+        print(f"\nC1 = {C1}")
+        print("This means that the initial concentration of your solution is", C1, "Molarity units.")
+        
+    elif choice == "2":
+        C1 = float(input("Enter C1: "))
+        C2 = float(input("Enter C2: "))
+        V2 = float(input("Enter V2: "))
+        V1 = Calculate_V1(C1, C2, V2)
+        print(f"\nV1 = {V1}")
+        print("This means that the initial volume of your solution is", V1, "volume units.")
+        
+    elif choice == "3":
+        C1 = float(input("Enter C1: "))
+        V1 = float(input("Enter V1: "))
+        V2 = float(input("Enter V2: "))
+        C2 = Calculate_C2(C1, V1, V2)
+        print(f"\nC2 = {C2}")
+        print("This means that the final concentration of your solution is", C2, "Molarity units.")
+        
+    elif choice == "4":
+        C1 = float(input("Enter C1: "))
+        V1 = float(input("Enter V1: "))
+        C2 = float(input("Enter C2: "))
+        V2 = Calculate_V2(C1, V1, C2)
+        print(f"\nV2 = {V2}")
+        print("This means that the final volume of your solution is", V2, "Volume units.")
