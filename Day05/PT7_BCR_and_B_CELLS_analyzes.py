57b3203a23230172edcefbcc3f644af311e19d77
Or Blank <your-email@example.com>
2026-05-15 18:20:47 +0300

    ranked is being saved as an excel file

--- Day05/PT7_BCR_and_B_CELLS_analyzes.py
+++ Day05/PT7_BCR_and_B_CELLS_analyzes.py
@@ -55,6 +55,7 @@ def plot_table(df, title, filename):
     table.auto_set_font_size(False)
     table.set_fontsize(10)
     table.scale(1.2, 1.2)
+
     for (i, j), cell in table.get_celld().items():
         if i == 0:
             cell.set_text_props(fontweight='bold')
@@ -76,8 +77,9 @@ print(clone_cluster_tables["PT7_Nm57_127_A1A2G1G2G3M"]) #Also here, if you want 
 
 print("\nNote: The above tables are being saved as images in your folder, please wait.")
 
-# Generate table images
+# Generate table images and excel file
 plot_table(global_cluster, "Cell Type Percentages of all Cells", "Day05/global_clusters.png")
 plot_table(clone_cluster_tables["PT7_Nm57_127_A1A2G1G2G3M"], "Cluster table for clone PT7_Nm57_127_A1A2G1G2G3M", "Day05/example_clone.png")
+clone_rank.to_excel("Day05/clone_rank.xlsx", index=True)
 
-print("\nTable images saved to Day05/ directory.") #Change the Day05/ directory to the name of the folder you are working in if you are not working in a folder named Day05
+print("\nTable images and excel file saved to Day05/ directory.") #Change the Day05/ directory to the name of the folder you are working in if you are not working in a folder named Day05
