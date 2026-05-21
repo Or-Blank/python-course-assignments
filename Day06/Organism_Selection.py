a7a1968018ddf0c78379129f5a50e6eb084f0ad1
Or-Blank <orbl825@gmail.com>
2026-05-21 11:48:03 +0300

    Update comment to include example organism names

--- Day06/Organism_Selection.py
+++ Day06/Organism_Selection.py
@@ -1,3 +1,3 @@
-#Enter the names of two organisms that you would like to compare:
+#Enter the names of two organisms that you would like to compare, I provided an example:
 ORG1 = "3505" # White birch
 ORG2 = "4039"  # Carrot / Queen Anne's lace
