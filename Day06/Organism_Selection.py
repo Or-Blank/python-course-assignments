3d32b1f91c914fbcde8c81a0daf7785b294d0e07
Or Blank <your-email@example.com>
2026-05-21 11:46:13 +0300

    updated the organism example

--- Day06/Organism_Selection.py
+++ Day06/Organism_Selection.py
@@ -1,3 +1,3 @@
 #Enter the names of two organisms that you would like to compare:
-ORG1 = "51240" # White birch
-ORG2 = "3818"  # Carrot / Queen Anne's lace
+ORG1 = "3505" # White birch
+ORG2 = "4039"  # Carrot / Queen Anne's lace
